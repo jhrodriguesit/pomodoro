2c8ffe5ba06f16cbc5c8605b64c2b019bdb43ad4
Joao Henrique <jhrodriguesit@gmail.com>
2026-05-02 00:16:01 +1000

    feat: add genre data and timer constants

---
+++ src/constants.js
@@ -0,0 +1,13 @@
+export const GENRES = {
+  reggae:    { label: 'Reggae',     videoId: 'zODPkMZ2pvw' },
+  pop:       { label: 'Pop',        videoId: 'KJyskZhXJHA' },
+  classical: { label: 'Classical',  videoId: 'RiG_fsPmNu4' },
+  jazz:      { label: 'Jazz',       videoId: 'pyNiTUvNxxE' },
+  bossanova: { label: 'Bossa Nova', videoId: 'LTyUGpq-1MY' },
+}
+
+export const GENRE_ORDER = ['reggae', 'pop', 'classical', 'jazz', 'bossanova']
+
+export const FOCUS_DURATION = 25 * 60   // 1500 seconds
+export const BREAK_DURATION = 5 * 60    // 300 seconds
+export const TOTAL_CYCLES = 4
